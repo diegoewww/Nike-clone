c83df5d97b83c166192294671d18b65506919e4d
tuvieja <105611148+n1na666@users.noreply.github.com>
2023-11-17 15:03:36 -0500

    first commit

---
+++ src/components/ShoeCard.jsx
@@ -0,0 +1,17 @@
+
+
+const ShoeCard = ({ shoe, changeImage, bigShoeImg }) => {
+  const handleClick = () => {
+    if (bigShoeImg !== shoe.bigShoe) changeImage(shoe.bigShoe)
+  }
+  return (
+    <div className={`border-2 rounded-xl ${bigShoeImg === shoe.bigShoe ? 'border-coral-red' : 'border-transparent'} cursor-pointer max-sm:flex-1`}
+      onClick={handleClick}>
+      <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+        <img src={shoe.thumbnail} alt="shoecolection" width={127} height={103} className="object-contain" />
+      </div>
+    </div>
+  )
+}
+
+export default ShoeCard
